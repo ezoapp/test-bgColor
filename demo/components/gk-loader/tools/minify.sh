443091bd2705d2accb335ab3cb7cdeed60248b6d
oxxo <oxxo.studio@gmail.com>
2014-04-15 11:41:56 +0800

    edited demo file

--- demo/components/gk-loader/tools/minify.sh
+++
@@ -1,3 +0,0 @@
-uglifyjs ../gk-loader.js -c -m > ../gk-loader.min.js
-uglifyjs ../element/loader.js -c -m -r keys,gk > ../element/loader.min.js
-uglifyjs ../widget/loader.js -c -m > ../widget/loader.min.js
